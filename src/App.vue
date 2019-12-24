07b9a3164df7673c8000b5ba830ef8ba8d5e03d6
ruud17 <mehmedjusic@gmail.com>
2019-12-24 02:02:05 +0100

    Code refreshed

--- src/App.vue
+++ src/App.vue
@@ -18,20 +18,4 @@ export default {
 </script>
 
 <style lang="scss">
-body {
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-  min-height: 100vh;
-  // background-image: url('./assets/bg.jpeg');
-  // opacity: 0.4;
-}
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 </style>
